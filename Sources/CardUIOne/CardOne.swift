7ea99c79832c6b99982af2ba2394ac1e155a0333
Damon Gonzalez <gonzowonzo5000@gmail.com>
2023-09-05 14:35:58 -0700

    ADDED: Option to make Card Art Holographic, Rarity option and set logo color change based on rarity, flavor text, collection number with max card count option, artist name, trademark/copy right with company name, company website

--- Sources/CardUIOne/CardOne.swift
+++ Sources/CardUIOne/CardOne.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
+@available(iOS 13.0, *)
 public struct CardOne: View {
     public var cardTitle:String
     public var cardColorElement:Color
@@ -39,33 +40,62 @@ public struct CardOne: View {
     public var cardType:String
     public var cardSubType:String
     public var cardExpansionSymbol:String
+    public var cardRarity:Rarity
     public var cardTextBox:String
+    public var cardFlavorText:String?
     public var cardPower:Int
     public var cardToughness:Int
+    public var cardHolo:Bool
+    @State private var animateGradient:Bool = false
+    public var cardCollectionNumber:Int
+    public var cardCollectionNumberMax:Int
+    public var cardArtist:String
+    public var cardYear:Int
+    public var cardCompanyName:String
+    public var website:String?
     public init(
         cardTitle: String = "Lorem ipsum dolor sit",
-        cardColorElement: Color = .cardRed,
+        cardColorElement: Color = .cardBlue,
         cardColorManaAmount: Int = 2,
         cardColorlessManaAmount: Int = 2,
         cardArt: String = "",
         cardType: String = "Lorem",
         cardSubType: String = "Ipsum",
         cardExpansionSymbol: String = "",
+        cardRarity: Rarity = .uncommon,
         cardTextBox: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel facilisis urna. Nam sit amet vulputate eros. Quisque tempus, quam ut euismod maximus, massa magna placerat nulla, vel posuere arcu.- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel facilisis urna. Nam sit amet vulputate eros. Quisque tempus, quam ut euismod maximus, massa magna placerat nulla, vel posuere arcu.",
+        cardFlavorText: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam ut euismod maximus. - Quisque",
         cardPower: Int = 1,
-        cardToughness: Int = 1) {
-        self.cardTitle = cardTitle
-        self.cardColorElement = cardColorElement
-        self.cardColorManaAmount = cardColorManaAmount
-        self.cardColorlessManaAmount = cardColorlessManaAmount
-        self.cardArt = cardArt
-        self.cardType = cardType
-        self.cardSubType = cardSubType
-        self.cardExpansionSymbol = cardExpansionSymbol
-        self.cardTextBox = cardTextBox
-        self.cardPower = cardPower
-        self.cardToughness = cardToughness
-    }
+        cardToughness: Int = 1,
+        cardHolo: Bool = false,
+        cardCollectionNumber: Int = 151,
+        cardCollectionNumberMax: Int = 151,
+        cardArtist:String = "Damon Gonzalez",
+        cardYear: Int = 2023,
+        cardCompanyName: String = "Dwarves of Mordor",
+        website: String = "dwarvesofmordor.com"){
+            self.cardTitle = cardTitle
+            self.cardColorElement = cardColorElement
+            self.cardColorManaAmount = cardColorManaAmount
+            self.cardColorlessManaAmount = cardColorlessManaAmount
+            self.cardArt = cardArt
+            self.cardType = cardType
+            self.cardSubType = cardSubType
+            self.cardExpansionSymbol = cardExpansionSymbol
+            self.cardRarity = cardRarity
+            self.cardTextBox = cardTextBox
+            self.cardFlavorText = cardFlavorText
+            self.cardPower = cardPower
+            self.cardToughness = cardToughness
+            self.cardHolo = cardHolo
+            self.cardCollectionNumber = cardCollectionNumber
+            self.cardCollectionNumberMax = cardCollectionNumberMax
+            self.cardArtist = cardArtist
+            self.cardYear = cardYear
+            self.cardCompanyName = cardCompanyName
+            self.website = website
+        }
+    
     public var body: some View {
         ZStack {
             Rectangle()
@@ -88,7 +118,7 @@ public struct CardOne: View {
                         .cornerRadius(10)
                         .foregroundColor(.areaBg)
                     Text(cardTitle)
-                        //.monospaced().bold()
+                    //.monospaced().bold()
                         .aspectRatio(contentMode: .fit)
                         .minimumScaleFactor(0.01)
                         .lineLimit(1)
@@ -427,11 +457,34 @@ public struct CardOne: View {
                         .frame(maxWidth: 350,maxHeight: 285)
                         .cornerRadius(15)
                         .foregroundColor(.areaBg)
-                    Image(cardArt)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: 350,maxHeight: 285)
-                        .cornerRadius(15)
+                    switch cardHolo {
+                    case true:
+                        ZStack {
+                            Image(cardArt)
+                                .resizable()
+                                .cornerRadius(15)
+                                .frame(maxWidth: 350,maxHeight: 285)
+                                .clipShape(Rectangle())
+                            LinearGradient(colors: [.holographic1,.holographic2,.holographic3,.holographic4,.holographic5], startPoint: .topLeading, endPoint: .bottomTrailing)
+                                .hueRotation(.degrees(animateGradient ? 45 : 0))
+                                .cornerRadius(15)
+                                .frame(maxWidth: 350,maxHeight: 285)
+                                .clipShape(Rectangle())
+                                .onAppear {
+                                    withAnimation(.linear(duration: 0.8).repeatForever(autoreverses: true)) {
+                                        animateGradient.toggle()
+                                    }
+                                }
+                                .opacity(0.5)
+                        }
+                    case false:
+                        Image(cardArt)
+                            .resizable()
+                            .cornerRadius(15)
+                            .frame(maxWidth: 350,maxHeight: 285)
+                            .clipShape(Rectangle())
+                    }
+                    
                 }
                 //TYPE - SET
                 ZStack {
@@ -445,20 +498,56 @@ public struct CardOne: View {
                         .cornerRadius(10)
                         .foregroundColor(.areaBg)
                     Text(cardType + " - " + cardSubType)
-                        //.monospaced().bold()
+                    //.monospaced().bold()
                         .minimumScaleFactor(0.01)
                         .lineLimit(1)
                     //.border(Color.green)
                         .frame(maxWidth: 200, alignment: .leading)
                     //.border(Color.red)
                         .offset(x:-75)
-                    Image(cardExpansionSymbol)
-                        .resizable()
-                        .scaledToFit()
-                    //.border(Color.red)
-                        .frame(maxWidth:40,maxHeight: 26, alignment: .center)
-                    //.border(Color.green)
-                        .offset(x:155)
+                    switch cardRarity {
+                    case .common:
+                        Image(cardExpansionSymbol)
+                            .resizable()
+                            .scaledToFit()
+                            .colorInvert()
+                            .colorMultiply(.common)
+                        //.border(Color.red)
+                            .frame(maxWidth:40,maxHeight: 26, alignment: .center)
+                        //.border(Color.green)
+                            .offset(x:165)
+                    case .uncommon:
+                        Image(cardExpansionSymbol)
+                            .resizable()
+                            .scaledToFit()
+                            .colorInvert()
+                            .colorMultiply(.uncommon)
+                        //.border(Color.red)
+                            .frame(maxWidth:40,maxHeight: 26, alignment: .center)
+                        //.border(Color.green)
+                            .offset(x:165)
+                    case .rare:
+                        Image(cardExpansionSymbol)
+                            .resizable()
+                            .scaledToFit()
+                            .colorInvert()
+                            .colorMultiply(.rare)
+                        //.border(Color.red)
+                            .frame(maxWidth:40,maxHeight: 26, alignment: .center)
+                        //.border(Color.green)
+                            .offset(x:165)
+                    case .mythicRare:
+                        Image(cardExpansionSymbol)
+                            .resizable()
+                            .scaledToFit()
+                            .colorInvert()
+                            .colorMultiply(.mythicRare)
+                        //.border(Color.red)
+                            .frame(maxWidth:40,maxHeight: 26, alignment: .center)
+                        //.border(Color.green)
+                            .offset(x:165)
+                    }
+                    
                 }
                 //TEXT
                 ZStack {
@@ -472,13 +561,19 @@ public struct CardOne: View {
                         .cornerRadius(15)
                         .foregroundColor(.areaBg)
                     Text(cardTextBox)
-                        //.monospaced().bold()
+                    //.monospaced().bold()
                         .minimumScaleFactor(0.01)
                         .lineLimit(10)
-                    //.border(Color.green)
-                        .frame(maxWidth: 340,maxHeight: 160, alignment: .leading)
-                    //.border(Color.red)
+                        .frame(maxWidth: 340,maxHeight: 160, alignment: .center)
+                        .offset(y:-15)
+                    Text(cardFlavorText ?? "")
+                        .font(.custom("BodoniSvtyTwoITCTT-BookIta", size: 20))
+                        .minimumScaleFactor(0.01)
+                        .lineLimit(2)
+                        .frame(maxWidth: 330,maxHeight: 20, alignment: .leading)
+                        .offset(y:75)
                 }
+                
             }
             //LIFE - ATTACK
             ZStack {
@@ -492,11 +587,41 @@ public struct CardOne: View {
                     .cornerRadius(5)
                     .foregroundColor(.areaBg)
                 Text("\(cardPower)" + "/" + "\(cardToughness)")
-                    //.font(.system(size: 15).monospaced().bold())
+                //.font(.system(size: 15).monospaced().bold())
                     .lineLimit(1)
                     .foregroundColor(.black)
             }
             .offset(x:165,y:295)
+            //COLLECTOR NUMBER - ARTIST NAME
+            VStack(alignment:.leading) {
+                Text("\(cardCollectionNumber)" + "/" + "\(cardCollectionNumberMax)")
+                    .minimumScaleFactor(0.01)
+                    .foregroundColor(.white)
+                    .font(.system(size: 5))
+                    .lineLimit(1)
+                Text("Artist: " + cardArtist)
+                    .minimumScaleFactor(0.01)
+                    .foregroundColor(.white)
+                    .font(.system(size: 5))
+                    .lineLimit(1)
+            }
+            .frame(maxWidth: 140,alignment: .leading)
+            .offset(x:-110,y:315)
+            //TRADMARK/COPYRIGHT - WEBSITE
+            VStack(alignment:.trailing) {
+                Text("™ & © " + "\(cardYear)" + cardCompanyName)
+                    .minimumScaleFactor(0.01)
+                    .foregroundColor(.white)
+                    .font(.system(size: 5))
+                    .lineLimit(1)
+                Text(website ?? "")
+                    .minimumScaleFactor(0.01)
+                    .foregroundColor(.white)
+                    .font(.system(size: 5))
+                    .lineLimit(1)
+            }
+            .frame(maxWidth: 140,alignment: .trailing)
+            .offset(x:60,y:315)
         }
     }
 }
@@ -505,7 +630,12 @@ public struct CardOne: View {
 
 
 
-
+public enum Rarity {
+    case common
+    case uncommon
+    case rare
+    case mythicRare
+}
 
 @available(iOS 13.0, *)
 public extension Color {
@@ -525,6 +655,15 @@ public extension Color {
     static let cardPurpleBG = Color(red: 96.0 / 255, green: 40.0 / 255, blue: 142.0 / 255)
     static let cardBlackBG = Color(red: 59.0 / 255, green: 59.0 / 255, blue: 59.0 / 255)
     static let cardWhiteBG = Color(red: 217.0 / 255, green: 217.0 / 255, blue: 217.0 / 255)
+    static let holographic1 = Color(red: 34.0 / 255, green: 157.0 / 255, blue: 225.0 / 255)
+    static let holographic2 = Color(red: 117.0 / 255, green: 237.0 / 255, blue: 225.0 / 255)
+    static let holographic3 = Color(red: 207.0 / 255, green: 199.0 / 255, blue: 250.0 / 255)
+    static let holographic4 = Color(red: 255.0 / 255, green: 160.0 / 255, blue: 157.0 / 255)
+    static let holographic5 = Color(red: 255.0 / 255, green: 244.0 / 255, blue: 185.0 / 255)
+    static let common = Color(red: 0.0 / 255, green: 0.0 / 255, blue: 0.0 / 255)
+    static let uncommon = Color(red: 117.0 / 255, green: 117.0 / 255, blue: 117.0 / 255)
+    static let rare = Color(red: 170.0 / 255, green: 150.0 / 255, blue: 75.0 / 255)
+    static let mythicRare = Color(red: 229.0 / 255, green: 87.0 / 255, blue: 8.0 / 255)
 }
 
 @available(iOS 13.0, *)
